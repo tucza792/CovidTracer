2bee22d6124141ec825c717b7e2f99689e8fd993
tucza792 <tucza792@student.otago.ac.nz>
2020-08-23 19:41:48 +1200

    Created basic webpage

---
+++ www/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML//EN">
+<html>
+	<head>
+		<title>Covid-19 Tracer</title>
+	</head>
+
+	<body>
+		<h1>Covid-19 Tracer</h1>
+
+		<h2>Input visit details</h2>
+	</body>
+</html>
