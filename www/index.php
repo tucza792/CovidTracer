286db0f6dcd124b793f9556c6047776b7c9b2532
tucza792 <tucza792@student.otago.ac.nz>
2020-08-24 19:38:45 +1200

    Added form for visit information along with button to submit info to the database

--- www/index.php
+++ www/index.php
@@ -9,11 +9,6 @@
 
 		<h2>Input visit details</h2>
 
-		<p>Showing contents of contact table:</p>
-
-		<table border="2">
-		<tr><th>First Name</th><th>Last Name</th><th>Email Address</th><th>Date and Time of Visit</th></tr>
-
 		<?php
 
 		 $db_host = '192.168.2.12';
@@ -22,16 +17,65 @@
 		 $db_passwd = 'insecure_db_pw';
 
 		 $pdo_dsn = "mysql:host=$db_host;dbname=$db_name";
-
 		 $pdo = new PDO($pdo_dsn, $db_user, $db_passwd);
+		 
 
-		 $q = $pdo->query("SELECT * FROM contact");
-
-		while($row = $q->fetch()) {
-		    echo "<tr><td>".$row["fname"]."</td><td>".$row["lname"]."</td><td>".$row["email"]."</td><td>".$row["time_of_visit"]."</td></tr>\n";
-		}
-
+		if(isset($_POST['add'])) {
+			$fname = $_POST['fname'];
+			$lname = $_POST['lname'];
+			$email = $_POST['email'];
+			$time_of_visit = $_POST['time_of_visit'];
+  
+			$q = $pdo->query("INSERT INTO contact VALUES ('$fname','$lname','$email','$time_of_visit')");
+			$q->fetch();
+			  
+		  }
 		?>
+
+		<form method = "post" action = "<?php $_PHP_SELF ?>">
+			<table width = "400" border = "0" cellspacing = "1" cellpadding = "2">
+			<tr>
+				<td width = "100">First Name</td>
+				<td><input name = "fname" type = "text" id = "fname"></td>
+			</tr>
+			<tr>
+				<td width = "100">Last Name</td>
+				<td><input name = "lname" type = "text" id = "lname"></td>
+			</tr>
+			<tr>
+				<td width = "100">Email Address</td>
+				<td><input name = "email" type = "text" id = "email"></td>
+			</tr>
+			<tr>
+				<td width = "100">Date and Time of Visit</td>
+				<td><input name = "time_of_visit" type = "text" id = "time_of_visit"></td>
+			</tr>
+			<tr>
+				<td width = "100"> </td>
+				<td> </td>
+			</tr>
+
+			<tr>
+				<td width = "100"> </td>
+				<td>
+				<input name = "add" type = "submit" id = "add" value = "Add Visit">
+				</td>
+			</tr>
+			</table>
+        </form>
+
+		<p>Showing contents of contact table:</p>
+
+		<table border="2">
+		<tr><th>First Name</th><th>Last Name</th><th>Email Address</th><th>Date and Time of Visit</th></tr>
+
+		  <?php
+		  $q = $pdo->query("SELECT * FROM contact");
+
+		  while($row = $q->fetch()) {
+			  echo "<tr><td>".$row["fname"]."</td><td>".$row["lname"]."</td><td>".$row["email"]."</td><td>".$row["time_of_visit"]."</td></tr>\n";
+		  }
+		  ?>
 		</table>
 	</body>
 
