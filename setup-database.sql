ab5f9da518cd921f938668777300ed2eed54053e
tucza792 <tucza792@student.otago.ac.nz>
2020-08-24 19:39:03 +1200

    Removed insert of test data

--- setup-database.sql
+++ setup-database.sql
@@ -2,7 +2,7 @@ CREATE TABLE contact (
        fname VARCHAR(20) NOT NULL,
        lname VARCHAR(20) NOT NULL,
        email VARCHAR(50) PRIMARY KEY,
-       time_of_visit DATETIME NOT NULL
+       time_of_visit VARCHAR(50) NOT NULL
 );
 
-INSERT INTO contact VALUES ('Testy', 'McTestface', 'mctestface@gmail.com', '1999-12-18 13:17:17');
+
